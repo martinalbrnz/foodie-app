3bdd4d5cda5b6a23514c1d9ae1f29e54e017a96d
martinalbrnz <martinalbor96@gmail.com>
2022-07-10 15:00:27 -0300

    Add style to recipe cards, changed to material icons

--- src/Components/Home/RecipeCard/index.jsx
+++ src/Components/Home/RecipeCard/index.jsx
@@ -1,25 +1,29 @@
 import styles from './recipeCard.module.css';
 
-const RecipeCard = () => {
-	return (
-		<div className={styles.cardContainer}>
-			<h2>Mozzarella pizza</h2>
-			<div className={styles.estimatedTime}>
-				<span className="material-symbols-outlined">
-					schedule
-				</span>
-				<p>45m</p>
-			</div>
-			<span class="material-symbols-outlined">
-				favorite
-			</span>
-			<div className={styles.recipeImage}>IMAGE</div>
-			<p>6 ingredients</p>
-			<p>6 utensils</p>
-			<p>Vegetarian</p>
-			<a href='#'>TRY IT</a>
-		</div>
-	);
+const RecipeCard = (props) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardHeader}>
+        <h2>Mozzarella pizza</h2>
+        <i class="material-icons">favorite{props.favorite ? '' : '_border'}</i>
+      </div>
+      <div className={styles.estimatedTime}>
+        <i className="material-icons">
+          schedule
+        </i>
+        <p>45m</p>
+      </div>
+      <div className={styles.recipeImage}></div>
+      <div className={styles.cardFooter}>
+        <div className={styles.infoContainer}>
+          <p>6 ingredients</p>
+          <p>6 utensils</p>
+          <p>Vegetarian</p>
+        </div>
+        <p className={styles.tryButton}>TRY IT</p>
+      </div>
+    </div>
+  );
 }
 
 export default RecipeCard;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,18 +2,20 @@ import styles from './home.module.css';
 import RecipeCard from './RecipeCard';
 
 const Home = () => {
-	// SEARCHBAR
-	// SIDEBAR
-	// RECIPE CARDS
-	return <div className={styles.home}>
-		<RecipeCard />
-		<RecipeCard />
-		<RecipeCard />
-		<RecipeCard />
-		<RecipeCard />
-		<RecipeCard />
-		<RecipeCard />
-	</div>;
+  // SEARCHBAR
+  // SIDEBAR
+  // RECIPE CARDS
+  return <div className={styles.home}>
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={false} />
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={false} />
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={true} />
+    <RecipeCard favorite={true} />
+  </div>;
 }
 
 export default Home;
